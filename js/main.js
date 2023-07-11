bfa8d43ec4d620e1a1032cb6c0c81cbc924eeac7
Beaka0717 <96518262+Beaka0717@users.noreply.github.com>
2023-07-11 11:06:03 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 
-
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function (){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function (){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
 
 
 const badgeEl = document.querySelector('header .badges');
@@ -71,3 +55,4 @@ spyEls.forEach(function(spyEl){
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 })
+
